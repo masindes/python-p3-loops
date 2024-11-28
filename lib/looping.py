a65b54df4c0709a8a324f172c23b47f40b1ce9b8
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-28 14:45:25 +0300

    initial commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+     # write your code here
+    countdown =10
+    while countdown in range(10,0,-1):
+     print(countdown)
+    countdown -=1
+print("Happy New Year!")
+
+    
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+     # write your code here
+     return [i ** 2 for i in int_list]
+
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # write your code here
+    for i in range(1,101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
